cf6da0e1787de019867bc21e219521b50daef21d
muhammedirfan07 <muhammadirfank645@gmail.com>
2026-06-14 23:02:02 +0530

    context

--- src/context/ContextAPI.jsx
+++ src/context/ContextAPI.jsx
@@ -3,25 +3,19 @@ import React, { createContext, useState } from "react";
 export const addStaionResponseContext =createContext()
 export const updateStaionResponseContext =createContext()
 export const reviewAndRatingContext= createContext()
-export const addVehicleResponseContext = createContext()
-export const updateVehicleResponseContext = createContext()
+
 
 const Contextapi = ({children})=>{
     const[addStaionResponse,setAddStaionResponse]=useState("")
     const[updateStaionResponse,setUpdateStaionResponse]=useState("")
     const[reviewStaionResponse,setReviewStaionResponse]=useState("")
-    const [addVehicleResponse,setAddVehicleResponse]=useState("")
-     const [updateVehicleResponse, setUpdateVehicleResponse] = useState("")
+
 
     return (
        <reviewAndRatingContext.Provider value={{reviewStaionResponse,setReviewStaionResponse}} >
            <updateStaionResponseContext.Provider value={{updateStaionResponse,setUpdateStaionResponse}}>
-                <addStaionResponseContext.Provider value={{addStaionResponse,setAddStaionResponse}}>
-                    <addVehicleResponseContext.Provider value={{addVehicleResponse,setAddVehicleResponse}}>
-                       <updateStaionResponseContext.Provider value={{updateVehicleResponse,setUpdateVehicleResponse}}> 
-                        {children}
-                        </updateStaionResponseContext.Provider>
-                    </addVehicleResponseContext.Provider>
+                <addStaionResponseContext.Provider value={{addStaionResponse,setAddStaionResponse}}>   
+                        {children}   
                 </addStaionResponseContext.Provider>
            </updateStaionResponseContext.Provider>
        </reviewAndRatingContext.Provider>
